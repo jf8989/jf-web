264426cac3c4e31a62d8401ea09654946c18f904
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2025-04-24 05:11:08 -0500

    update track list and names in AudioPlayer component for improved organization

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -86,19 +86,19 @@ const AudioPlayer: React.FC = () => {
   // Playlist
   const trackList = useMemo(
     () => [
+      "/audio/UwunuInstrumental-JF-Master.mp3",
+      "/audio/Oddyssey-JF-Master.mp3",
       "/audio/BBC4-Instrumental-JF-Master.mp3",
       "/audio/FaceTheNight-Mazure-JF-Master.mp3",
-      "/audio/Oddyssey-JF-Master.mp3",
-      "/audio/UwunuInstrumental-JF-Master.mp3",
     ],
     []
   );
   const trackNames = useMemo(
     () => [
+      "Uwunu - Shani [Instrumental] (JF Master)",
+      "Oddyssey (JF Master)",
       "BBC4 - Francu [Instrumental] (JF Master)",
       "Face The Night - Mazure (JF Master)",
-      "Oddyssey (JF Master)",
-      "Uwunu - Shani [Instrumental] (JF Master)",
     ],
     []
   );
